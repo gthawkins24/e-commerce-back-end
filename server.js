e2062e86cdc64dd6d36b987033a1f621ff11850f
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-03-03 21:03:20 -0500

    setup connection to database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+// const { sequelize } = require('./models/Product');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -11,6 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
+sequelize.sync({ force: true })
+.then(() => {
+  app.listen(PORT, () => console.log(`Now listening on Port: ${PORT}`));
 });
